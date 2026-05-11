ce7dbd08ed75fed0d6b030982eaf85e7c6f430e9
rocketsaymon-xyz <rocketsaymon@gmail.com>
2026-05-11 04:34:28 +0100

    added task 4 and 5,

--- sql/explore_donor_user_donation.sql
+++ sql/explore_donor_user_donation.sql
@@ -11,7 +11,8 @@
 1. структуру данных;
 2. пропуски;
 3. технические дубликаты;
-4. влияние дублей на сумму donation_count.
+4. Проверить типы данных;
+5. Проверить на выбросы.
 
 Бизнес-смысл:
 Перед расчётом активности доноров и бонусов нужно убедиться,
@@ -60,14 +61,15 @@ WHERE
 	cnt_double > 1
 -- Дубликаты есть;
 	
--- 4. Оценка масштаба дубликатов
+-- Оценка масштаба дубликатов
 WITH double_line AS (
 	SELECT
 		*
 		, ROW_NUMBER() OVER(
 	PARTITION BY donor_id, event_id, donation_data, donation_count
 	ORDER BY id) AS cnt_double
-	FROM donorsearch.user_donation ud
+	FROM
+		donorsearch.user_donation ud
 )
 SELECT 
 	COUNT(*) AS total_rows
@@ -84,9 +86,35 @@ FROM
 	double_line
 	-- 3031 дубликат из 6231. Трубуется удаление.;/*
 
+-- 4. Проверяем типы данных в таблице
+SELECT 
+	column_name
+	, data_type
+FROM
+	information_schema."columns"
+WHERE
+	table_schema = 'donorsearch'
+	AND table_name = 'user_donation'
+	-- типы данных соответствуют содержанию;
+	
+--5. Проверяем на выбросы
+SELECT 
+	MIN(donation_data) AS min_donation_data
+	, MAX(donation_data) AS max_donation_data
+	, MIN(donation_count) AS min_donation_count
+	, MAX(donation_count) AS max_donation_count
+	, AVG(donation_count) AS avg_donation_count
+	, PERCENTILE_CONT(0.5) 
+		WITHIN GROUP(ORDER BY donation_count) AS median_donation_count
+FROM donorsearch.user_donation
+-- donation_data, даты донации корректные
+-- donation_count количество донаций, нет отрицательного. максимум адеваткный. 
+-- Таблица требует очистки перед расчётом retention/churn.. средняя и медиана показывают крайне низкие значения. очень много нулей.
+
 ВЫВОДЫ:
 1. Обнаружено 3031 технических дубликата (~48.6% строк).
 2. Дубли существенно завышают сумму donation_count.
-3. Обнаружены пропуски в donation_count.
-4. Таблица требует очистки перед расчётом retention/churn.
+3. Обнаружены пропуски в donation_count. Таблица требует очистки перед расчётом retention/churn.
+4. Типы данных корректны, соответствуют содержанию
+5. donation_count. очень много нулевых донаций у доноров. Скорее всего есть группы без донаций. необходимо выяснить причину.
 */
